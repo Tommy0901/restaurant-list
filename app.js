26e25e5ba44efb9d900a2fd31f34fbf834ad4eed
Tommy0901 <ak710180@gmail.com>
2023-11-07 13:06:45 +0800

    project init

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+const restaurants = require('./public/jsons/restaurants.json').results
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  const keyword = req.query.keyword?.trim() // qurey.keyword 對應 index.hbs <input ... name="keyword" 
+  const matchedRestaurants = keyword ? restaurants.filter((restaurant) =>
+    Object.values(restaurant).some(property => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : restaurants
+  res.render('index', { restaurants: matchedRestaurants, keyword })
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurants.find((restaurant) => restaurant.id === Number(id))
+  res.render('show', { restaurant })
+})
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
